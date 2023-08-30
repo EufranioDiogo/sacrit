3eabb143b95e24caf1da9ee47c4e02da8b51d575
--global <--global>
2023-08-30 18:19:44 +0100

    chore: working in services

---
+++ js/service_section.js
@@ -0,0 +1,7 @@
+const serviceItens = {
+  "service-item-routing": {
+    title: "Roteamento",
+    description: "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
+    image: ""
+  }
+}
