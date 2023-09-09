b6d24b0809d5e5a43efc42a1ba0a5614b77a1674
EufranioDiogo <--global>
2023-09-09 23:23:44 +0100

    feat: a lot of changes

--- js/header.js
+++ js/header.js
@@ -1,10 +1,10 @@
 const headerSlide = [
   {
-    image: './assets/images/pexels-tima-miroshnichenko-5685765.jpg',
+    image: './assets/images/pexels-carlos-césar-2767923.jpg',
     title: 'Nós o ajudamos a elevar e manter o seu negócio ao mais alto nível tecnológico'
   },
   {
-    image: './assets/images/pexels-thisisengineering-3861967.jpg',
+    image: './assets/images/pexels-tomás-sanimbo-3277188.jpg',
     title: 'Interligamos Gerações'
   },
   {
@@ -13,6 +13,7 @@ const headerSlide = [
   }
 ]
 const contentHeader = document.querySelector('.header-content-container--text')
+const header = document.querySelector('.image-header-container')
 let imageIndex = 0;
 
 setInterval(() => {
@@ -21,6 +22,7 @@ setInterval(() => {
     imageIndex = 0;
   }
 
+  //header.style.backgroundImage = `url('${headerSlide[imageIndex].image}')`;
   contentHeader.innerHTML = headerSlide[imageIndex].title;
 
 }, 6000);
--- js/service_section.js
+++ js/service_section.js
@@ -18,65 +18,167 @@ const serviceItens = {
         "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
       image: "../assets/images/pexels-brett-sayles-4339335.jpg",
     },
+    {
+      id: "service-item-3",
+      title: "Mobility",
+      icon: "../assets/icons/ion_wifi.svg",
+      activeIcon: "../assets/icons/ion_wifi.svg",
+      description: "Descrição da Mobility.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-4",
+      title: "Communications",
+      icon: "../assets/icons/carbon_network-4.svg",
+      activeIcon: "../assets/icons/carbon_network-4.svg",
+      description: "Descrição da Communications.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-5",
+      title: "Timing Synchronization",
+      icon: "../assets/icons/formkit_time.svg",
+      activeIcon: "../assets/icons/formkit_time.svg",
+      description: "Descrição do Timing Synchronization.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-6",
+      title: "Management and Orchestration",
+      icon: "../assets/icons/pajamas_container-image.svg",
+      activeIcon: "../assets/icons/pajamas_container-image.svg",
+      description: "Descrição do Management and Orchestration.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-7",
+      title: "Broadband Wireless Solutions",
+      icon: "../assets/icons/carbon_undefined.svg",
+      activeIcon: "../assets/icons/carbon_undefined.svg",
+      description: "Descrição do Broadband Wireless Solutions.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-8",
+      title: "Mobile Backhaul",
+      icon: "../assets/icons/carbon_undefined.svg",
+      activeIcon: "../assets/icons/carbon_undefined.svg",
+      description: "Descrição do Mobile Backhaul.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
   ],
   "Data Center": [
     {
-      id: "service-item-3",
+      id: "service-item-1",
       title: "Servidores",
       icon: "../assets/icons/bi_hdd-network-fill.svg",
       activeIcon: "../assets/icons/bi_hdd-network-fill.svg",
       description:
         "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
-      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
-    },
-    {
-      id: "service-item-4",
-      title: "Armazenamento",
-      icon: "../assets/icons/mdi_storage.svg",
-      activeIcon: "../assets/icons/mdi_storage.svg",
-      description:
-        "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
-      image: "../assets/images/pexels-brett-sayles-4597280.jpg",
-    },
-  ],
-  Networking1: [
-    {
-      id: "service-item-1",
-      title: "Roteamento",
-      icon: "../assets/icons/clarity_router-solid.svg",
-      activeIcon: "../assets/icons/clarity_router-solid.svg",
-      description:
-        "Temos a melhor solução de roteamento para a sua empresa, usando os mais diferentes e vendors.",
       image: "../assets/images/pexels-jah-nomad-17220238.jpg",
     },
     {
       id: "service-item-2",
-      title: "Switching",
-      icon: "../assets/icons/mi_switch.svg",
-      activeIcon: "../assets/icons/mi_switch.svg",
-      description:
-        "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
-      image: "../assets/images/pexels-brett-sayles-4339335.jpg",
-    },
-  ],
-  "Data Center1": [
-    {
-      id: "service-item-3",
-      title: "Servidores",
-      icon: "../assets/icons/bi_hdd-network-fill.svg",
-      activeIcon: "../assets/icons/bi_hdd-network-fill.svg",
-      description:
-        "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
-      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
-    },
-    {
-      id: "service-item-4",
       title: "Armazenamento",
       icon: "../assets/icons/mdi_storage.svg",
       activeIcon: "../assets/icons/mdi_storage.svg",
       description:
         "lorem lorem lorem lorem ipsum dolor sit amet, consectetur adip",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-3",
+      title: "Data Center",
+      icon: "../assets/icons/carbon_network-enterprise.svg",
+      activeIcon: "../assets/icons/carbon_network-enterprise.svg",
+      description: "Descrição de Data Center.",
       image: "../assets/images/pexels-brett-sayles-4597280.jpg",
+    },
+  ],
+  Colaboração: [
+    {
+      id: "service-item-1",
+      title: "Phone Systems",
+      icon: "../assets/icons/ic_sharp-phone.svg",
+      activeIcon: "../assets/icons/ic_sharp-phone.svg",
+      description: "Descrição do Phone Systems.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-2",
+      title: "MS",
+      icon: "../assets/icons/carbon_undefined.svg",
+      activeIcon: "../assets/icons/carbon_undefined.svg",
+      description: "Descrição do MS.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-3",
+      title: "Video Conferencing",
+      icon: "../assets/icons/mdi_virtual-meeting.svg",
+      activeIcon: "../assets/icons/mdi_virtual-meeting.svg",
+      description: "Descrição do Video Conferencing.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-4",
+      title: "Contact Center",
+      icon: "../assets/icons/mdi_customer-service.svg",
+      activeIcon: "../assets/icons/mdi_customer-service.svg",
+      description: "Descrição do Contact Center.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+  ],
+  "Infra-estrutura": [
+    {
+      id: "service-item-1",
+      title: "FTTx",
+      icon: "../assets/icons/carbon_undefined.svg",
+      activeIcon: "../assets/icons/carbon_undefined.svg",
+      description: "Descrição do FTTx.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-2",
+      title: "Cabling",
+      icon: "../assets/icons/ic_twotone-cable.svg",
+      activeIcon: "../assets/icons/ic_twotone-cable.svg",
+      description: "Descrição do Cabling.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-3",
+      title: "Optical Tx Solutions",
+      icon: "../assets/icons/flat-color-icons_cable-release.svg",
+      activeIcon: "../assets/icons/flat-color-icons_cable-release.svg",
+      description: "Descrição do Optical Tx Solutions.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+  ],
+  Security: [
+    {
+      id: "service-item-1",
+      title: "Gateway Protection",
+      icon: "../assets/icons/carbon_ibm-cloud-privileged-access-gateway.svg",
+      activeIcon: "../assets/icons/carbon_ibm-cloud-privileged-access-gateway.svg",
+      description: "Descrição do Gateway Protection.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-2",
+      title: "CCTV",
+      icon: "../assets/icons/icon-park-solid_surveillance-cameras-two.svg",
+      activeIcon: "../assets/icons/icon-park-solid_surveillance-cameras-two.svg",
+      description: "Descrição do CCTV.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
+    },
+    {
+      id: "service-item-3",
+      title: "Access Security",
+      icon: "../assets/icons/mdi_security-lock-outline.svg",
+      activeIcon: "../assets/icons/mdi_security-lock-outline.svg",
+      description: "Descrição do Access Security.",
+      image: "../assets/images/pexels-jah-nomad-17220238.jpg",
     },
   ],
 };
@@ -162,7 +264,7 @@ const createSection = ({ start = 0, end = 0 }) => {
     ".inner-section-2-container--control-container"
   );
 
-  sectionsContainer.innerHTML = '';
+  sectionsContainer.innerHTML = "";
 
   for (let i = start; i < end; i++) {
     const key = keysServiceItens[i];
@@ -197,7 +299,7 @@ moreButton.addEventListener("click", () => {
     actualStart = 0;
     moreButton.innerText = "Mais";
     showAll = false;
-    createSection({start: 0, end: 0});
+    createSection({ start: 0, end: 0 });
   } else {
     createSection({
       start: 0,
